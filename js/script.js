1294d909b25483c36cd2ce3840be2d7807f9957a
krasapchik <kutman_mamytov359@gmail.com>
2023-02-26 18:40:20 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -15,31 +15,37 @@ fetch(`https://v6.exchangerate-api.com/v6/41b4541f3df8b629ff6e1018/latest/${curr
 
 .then((res)=> res.json())
 .then((data)=>{
-    console.log(data);
+ console.log(data);
     const{conversion_rates} = data;
     dataFromBack ={...conversion_rates};
-
+     calculateRates(currencyTwo,conversion_rates);
+     rateE1.innerText =` 1${currenclyOne}=${conversion_rates[currencyTwo]} ${currencyTwo} `
 
 });
 };
  const calculateRates = (currenclTwo, data)=>{
     const rate = data[currenclTwo];
     amountTwoE1.value = (+amountOneE1.value * rate).toFixed(2)
+  
  }
 const  runCalculate = ()=> calculateRates(currenclyTwoE1.value, dataFromBack)
 
 getData()
-const swap =()=>{
+const swap = function(){
     const currenclyOne = currenclyOneE1.value;
     const currencyTwo = currenclyTwoE1.value;
-currenclyTwoE1.value = currenclyOne
-currenclyOneE1.value = currencyTwo
+    currenclyTwoE1.value = currenclyOne
+    currenclyOneE1.value = currencyTwo
 }
-swapE1.addEventListener('click', function(){
-    swap()
-  runCalculate()
-})
 
-amountOneE1.addEventListener('input',runCalculate);
-currenclyOneE1.addEventListener('change',getData);
-currenclyTwoE1.addEventListener('change', runCalculate);
+swapE1.addEventListener('click',function(){
+    swap()
+    runCalculate()
+    getData()
+
+   
+})
+amountOneE1.addEventListener("input", runCalculate);
+currenclyOneE1.addEventListener("change", getData);
+currenclyTwoE1.addEventListener("change", runCalculate);
+
